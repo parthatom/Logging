7a3691b94f9a651b6e508312ad6d7f897545bc21
Parth <parth.shxh@gmail.com>
2020-10-26 21:55:21 +0530

    Initial Commit

---
+++ Logger.py
@@ -0,0 +1,37 @@
+import os
+import numbers
+import numpy as np
+import pandas as pd
+
+class Logger():
+  def __init__(self, df_path, create = False, **kwargs):
+    self.kwargs = kwargs
+    if ("verbose" in self.kwargs):
+      self.verbose = self.kwargs['verbose']
+    else:
+      self.verbose = False
+    self.df_path = df_path
+    if (create):
+      self.df = pd.DataFrame(columns = ['val_accuracy'])
+    else:
+      self.df = pd.read_csv(self.df_path, index_col=False)
+    self.ind = len(self.df)
+    self.df.loc[self.ind] = np.nan
+
+  def log(self, key, value):
+    self.write_df(key, value, self.ind)
+
+  def write_df(self, key, value, ind):
+    if not key in self.df:
+      if (self.verbose):
+        print(key, "not in DataFrame, column has been added")
+      self.df[key]=np.nan
+      if (not isinstance(value, numbers.Number) ):
+        self.df[key] = self.df[key].astype(str)
+    self.df.at[ind, key] = value
+
+  def next(self):
+    self.ind += 1
+  
+  def save(self):
+    self.df.to_csv(self.df_path, index = False)
